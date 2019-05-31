ecc7e3bf536284e31acd6ff0d94a73fed7504cdf
VeroMorera <47680389+VeroMorera@users.noreply.github.com>
2019-05-31 16:35:51 -0600

    update tarea6

--- semana06/clinica_pacientes/tarea6_data.py
+++ semana06/clinica_pacientes/tarea6_data.py
@@ -46,22 +46,47 @@ while (salir == False):
                 medicina = input('Por favor digite la enfermedad ' + str(i + 1) + ' del paciente')
                 lista_medicinas.append(medicina)
 
+
+            #tools.la_clinica.agregar_paciente(**id)
+
+            # Definiendo pacientes
+            juanito = {'identificación': 1, 'Nombre': 'Juan', 'Apellido': 'Mora', 'teléfono': 24578451,
+                       'dirección': 'San Jose 3 st, 5 Av', 'lista_de_enfermedades_tratadas': ['dolor de cabeza'],
+                       'lista_de_medicamentos_que_toma': ['aspirina', 'panadol']}
+
+            carlitos = {'identificación': 2, 'Nombre': 'Carlos', 'Apellido': 'Fernandez', 'teléfono': 47845465,
+                        'dirección': 'Heredia 1 st, 3 Av', 'lista_de_enfermedades_tratadas': ['dolor de espalda'],
+                        'lista_de_medicamentos_que_toma': ['voltaren', 'panadol']}
+
+            anita = {'identificación': 3, 'Nombre': 'Ana', 'Apellido': 'Picado', 'teléfono': 4857848,
+                     'dirección': 'Cartago 5 st, 9 Av',
+                     'lista_de_enfermedades_tratadas': ['Fibroma', 'dolor de cabeza'],
+                     'lista_de_medicamentos_que_toma': ['voltaren', 'panadol']}
+
             id = {'identificación': id, 'Nombre': nombre, 'Apellido': apellido, 'teléfono': telefono,
                   'dirección': direccion, 'lista_de_enfermedades_tratadas': lista_enfermedades,
                   'lista_de_medicamentos_que_toma': lista_medicinas}
 
 
-            data['pacientes'].append(
-                tools.la_clinica.agregar_paciente(**id)
-            )
+            data['pacientes'].append(juanito)
+            data['pacientes'].append(carlitos)
+            data['pacientes'].append(anita)
+            data['pacientes'].append(id)
+
+
+        registrar_paciente()
+        print('')
+        print(data)
+
+        #salvar como json
+        with open('clinica_pacientes/base_datos_clinica.txt', 'w') as outfile:
+            json.dump(data, outfile)
+
 
     elif response == 2:
 
         def listar_paciente():
 
-
-            with open('clinica_pacientes/base_datos_clinica.txt', 'w') as outfile:
-                json.dump(data, outfile)
 
             # ABRIR BASE DE DATOS
             from pprint import pprint
@@ -90,7 +115,7 @@ while (salir == False):
 
 
     elif response == 6:
-        reporte_enfermedades_tratadas = la_clinica.enfermedades_tratadas()
+        reporte_enfermedades_tratadas = tools.la_clinica.enfermedades_tratadas()
         print(reporte_enfermedades_tratadas)
 
     elif response == 7:
